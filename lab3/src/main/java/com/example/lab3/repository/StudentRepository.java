c93897f44673121bc1efd01f083cc432235f3e63
bao238 <148884981+bao238@users.noreply.github.com>
2026-01-21 07:32:14 +0700

    Delete lab3 directory

--- lab3/src/main/java/com/example/lab3/repository/StudentRepository.java
+++
@@ -1,8 +0,0 @@
-package com.example.lab3.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import com.example.lab3.model.Student;
-
-public interface StudentRepository
-        extends JpaRepository<Student, Integer> {
-}
